1cd3a5f2913628e88a6c19b637916e800ab22a80
JojoELITE <jojoelite1014@gmail.com>
2024-07-22 17:55:17 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,15 @@ import React from 'react';
 import { PDFViewer } from '@react-pdf/renderer';
 import CvUn from './Components/CvUn';
 
+
 export default function Home() {
   return (
     <main className="w-screen h-screen">
+
       <PDFViewer className="h-full w-full" showToolbar={false}>
         <CvUn />
       </PDFViewer>
     </main>
   );
 }
+
